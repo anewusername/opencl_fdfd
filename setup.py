3cd26265bd7958d0ed6bf728462b8a336c64cb9e
Jan Petykiewicz <anewusername@gmail.com>
2018-09-16 20:14:42 -0700

    Use readme as long_description

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@
 from setuptools import setup, find_packages
 import opencl_fdfd
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(name='opencl_fdfd',
       version=opencl_fdfd.version,
       description='OpenCL FDFD solver',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Jan Petykiewicz',
       author_email='anewusername@gmail.com',
       url='https://mpxd.net/code/jan/opencl_fdfd',
